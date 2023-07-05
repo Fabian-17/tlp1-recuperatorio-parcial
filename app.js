15806f2686857fb7a927da06d23b0a6ac4344b56
Fabian <fabianvillagra28@gmail.com>
2023-07-05 14:28:01 -0300

    Se hicieron cambios necesacios para configurar el archivo de arranque del servidor

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
+const helmet = require('helmet');
+const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-
+require('ejs');
+app.set('view engine', 'ejs');
+const port = process.env.PORT || 5000;
 // Middlewares
 // TODO: Implementar middlewares
-
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
-
+app.use((req, res, next) => {
+    res.write(`<div>
+        <h1>404 - Ruta no encontrada</h1>
+        <hr>
+        <p>La pagina que intentas buscar no existe</p>
+        <p>Redireccionando a la página de inicio...</p>
+        <script>
+        (
+          () => setTimeout(() => {
+            window.location.href='http://localhost:${port}/';
+           }, 3000)           
+        )();
+        </script>
+    </h1>`)
+});
 // Starting the server
 app.listen(45635, () => console.log('Server on port xxxx'));
